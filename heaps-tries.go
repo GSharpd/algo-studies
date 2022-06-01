ae3003a61c4b3d33bbcbe894dfd3d1a837f66359
Gustavo Vieira <gustavo.vieira@betalearning.com.br>
2022-05-31 21:11:01 -0300

    theory on tries and heaps

---
+++ heaps-tries.go
@@ -0,0 +1,8 @@
+// Binary Heaps and Tries are forms of trees that are used to implement priority queues (queues that aren't necessarily FIFO)
+
+// In a Binary Heap, every node is greater than or equal to its children
+// similarly a binary heap can be a min heap or a max heap, depending on the value of the root node
+// if you insert a value to a binary heap and it is bigger than the root node, they have to be switched in order to maintain the validity of heap (rootNode > childrenNodes)
+// Note: the heap data structure is not to be confused with memory heap
+
+// a trie is a specialized tree focused on strings
